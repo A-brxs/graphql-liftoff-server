3e8a71788bc9926f75f216fc10c49c7d4411615d
Luis Z <luis@zuniga>
2023-03-04 18:31:44 +0200

    Initial commit server

---
+++ src/datasources/track-api.js
@@ -0,0 +1,31 @@
+const { RESTDataSource } = require("apollo-datasource-rest")
+
+class TrackAPI extends RESTDataSource {
+
+  constructor() {
+    super()
+    this.baseURL = "https://odyssey-lift-off-rest-api.herokuapp.com/"
+  }
+
+  getTracksForHome() {
+    return this.get('tracks')
+  }
+
+  getTrack(trackId) {
+    return this.get(`track/${encodeURIComponent(trackId)}`)
+  }
+
+  getTrackModules(trackId) {
+    return this.get(`track/${encodeURIComponent(trackId)}/modules`)
+  }
+
+  getAuthor(authorId) {
+    return this.get(`author/${encodeURIComponent(authorId)}`)
+  }
+
+  incrementTrackViews(trackId) {
+    return this.patch(`track/${trackId}/numberOfViews`);
+  }
+
+}
+module.exports = TrackAPI
---
+++ src/schema.js
@@ -0,0 +1,73 @@
+const { gql } = require('apollo-server')
+
+const typeDefs = gql`
+  # Schema definitions here
+  """
+  Track:
+  Will show the course's information for a particular Author.
+  """
+  type Track {
+    id: ID!
+    "Title of the course"
+    title: String!
+    "The author of this module"
+    author: Author!
+    "Thumbnail"
+    thumbnail: String
+    "Total length of all modules"
+    length: Int
+    "How many modules there are"
+    modulesCount: Int
+    "The track's complete description, can be in Markdown format"
+    description: String
+    "The number of times a track has been viewed"
+    numberOfViews: Int
+    "The list of Modules in this Track"
+    modules: [Module!]!
+  }
+  """
+  Author:
+  The creator of a module or modules.
+  """
+  type Author {
+    id: ID!
+    "Name of the Author"
+    name: String!
+    "Photo of the Author"
+    photo: String
+  }
+
+  "A Module is a single unit of teaching. Multiple Modules compose a Track"
+  type Module {
+    id: ID!
+    "The Module's title"
+    title: String!
+    "The Module's length in minutes"
+    length: Int
+  }
+
+  type Query {
+    "Get tracks array for homepage grid"
+    tracksForHome: [Track!]!
+    "Fetch a specific track, provided a track's ID"
+    track(id: ID!): Track
+  }
+
+  type Mutation {
+    "Increment number of track views"
+    incrementTrackViews(id: ID!): IncrementTrackViewsResponse! 
+  }
+
+  type IncrementTrackViewsResponse {
+    "Similar to HTTP status code, represents the status of the mutation"
+    code: Int!
+    "Indicates whether the mutation was successful"
+    success: Boolean!
+    "Human-readable message for the UI"
+    message: String!
+    "Newly updated track after a successful mutation"
+    track: Track
+  }
+`
+
+module.exports = typeDefs
